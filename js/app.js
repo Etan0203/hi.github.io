10835f6bdc1b70ec1bfb6242c8bbf7bb08d29b81
Etan0203 <122731818+Etan0203@users.noreply.github.com>
2023-01-15 20:46:55 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click',() =>{
+    question.innerHTML = 'I Love You Too Baby :) ';
+})
+
+noBtn.addEventListener('click',() =>{
+    question.innerHTML = 'I Love You Too Baby :) ';
+})
